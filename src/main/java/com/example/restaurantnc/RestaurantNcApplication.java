15b155e7d6e32a4348cf1cb2da35d5af6176443c
EugeneSergeevich20 <eugene.chernitsyn15@gmail.com>
2023-11-08 17:58:12 +0400

    First commit

---
+++ src/main/java/com/example/restaurantnc/RestaurantNcApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantNcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantNcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restaurantnc/RestaurantNcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantNcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
